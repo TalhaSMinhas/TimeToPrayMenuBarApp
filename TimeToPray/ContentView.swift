d12d005d464d7094a2480b51c5ffd859afe22bc6
TalhaSMinhas <talha.salman.minhas@icloud.com>
2025-08-21 21:47:11 +0200

    Initial Commit

---
+++ TimeToPray/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TimeToPray
+//
+//  Created by Talha Salman Minhas on 2025/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TimeToPray/TimeToPrayApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeToPrayApp.swift
+//  TimeToPray
+//
+//  Created by Talha Salman Minhas on 2025/08/21.
+//
+
+import SwiftUI
+
+@main
+struct TimeToPrayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
