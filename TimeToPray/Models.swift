e840f6b3b57e79ad371aff220ff8ee4385bd5734
TalhaSMinhas <talha.salman.minhas@icloud.com>
2025-08-21 23:58:15 +0200

    added models.swift and added json of times

---
+++ TimeToPray/Models.swift
@@ -0,0 +1,60 @@
+//
+//  Models.swift
+//  TimeToPray
+//
+//  Created by Talha Salman Minhas on 2025/08/21.
+//
+
+import Foundation
+
+struct DayTimes: Codable {
+    let sehri: String?
+    let subhSadiq: String?
+    let fajr: String?
+    let fajrAzaan: String?
+    let fajrJamaat: String?
+    let sunrise: String?
+    let ishraaq: String?
+    let chast: String?
+    let zawal: String?
+    let zuhr: String?
+    let zuhrAzaan: String?
+    let zuhrJamaat: String?
+    let jummahAzaan: String?
+    let jummah: String?
+    let asrShafi: String?
+    let asrHanafi: String?
+    let asrAzaan: String?
+    let sunset: String?
+    let maghrib: String?
+    let esha: String?
+    let eshaAzaan: String?
+    let eshaJamaat: String?
+
+    enum CodingKeys: String, CodingKey {
+        case sehri = "Sehri"
+        case subhSadiq = "Subh Sadiq"
+        case fajr = "Fajr"
+        case fajrAzaan = "Fajr Azaan"
+        case fajrJamaat = "Fajr Jamaat"
+        case sunrise = "Sunrise"
+        case ishraaq = "Ishraaq"
+        case chast = "Chast"
+        case zawal = "Zawal"
+        case zuhr = "Zuhr"
+        case zuhrAzaan = "Zuhr Azaan"
+        case zuhrJamaat = "Zuhr Jamaat"
+        case jummahAzaan = "Jummah Azaan"
+        case jummah = "Jummah"
+        case asrShafi = "Asr (Shafi)"
+        case asrHanafi = "Asr (Hanafi)"
+        case asrAzaan = "Asr Azaan"
+        case sunset = "Sunset"
+        case maghrib = "Maghrib"
+        case esha = "Esha"
+        case eshaAzaan = "Esha Azaan"
+        case eshaJamaat = "Esha Jamaat"
+    }
+}
+
+typealias Timetable = [String: [String: DayTimes]]
